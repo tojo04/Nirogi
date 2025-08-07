4ad3c76071504a937486dd7e18429e3dc0f7aa36
Rushil Jain <rushiljain69@gmail.com>
2025-08-07 19:39:40 +0530

    ADD: files in server

--- Server/Scrapers/tempCodeRunnerFile.py
+++
@@ -1,3 +0,0 @@
-                price_el = page.query_selector("div[id='PDP price banner'] p.tD")
-                price_text = price_el.inner_text().strip().replace("₹", "").replace(",", "") if price_el else None
-                price_val = float(price_text) if price_text else None
